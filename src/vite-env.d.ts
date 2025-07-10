d6036ef25222c2a0c268f537b92bc4e134a0b75b
Hongzheng Li <hongzheng@cs.wisc.edu>
2025-07-09 22:05:03 -0500

    style: format code with prettier

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly NODE_ENV: string
-  readonly REACT_APP_APP_NAME: string
-  readonly REACT_APP_MAPS_API_KEY: string
-  // Add other environment variables as needed
+    readonly NODE_ENV: string;
+    readonly REACT_APP_APP_NAME: string;
+    readonly REACT_APP_MAPS_API_KEY: string;
+    // Add other environment variables as needed
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
 }
